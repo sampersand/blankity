718996c6b7275757508fef726643fe4f87c965d7
SamW <mail@sampersand.me>
2025-10-20 11:07:37 -0700

    small updates

--- lib/blankity/blankslate.rb
+++ lib/blankity/blankslate.rb
@@ -1,38 +1,42 @@
 # frozen_string_literal: true
 
-class Blankity::BlankSlate < BasicObject
-  # Remove every method except for `__send__` and `__id__`
-  instance_methods.each do |name|
-    undef_method(name) unless name == :__send__ || name == :__id__
-  end
-
-  # A helper method to also include methods that are defined on `Kernel`
-  def __with_Kernel_method__(*methods)
-    dsm = ::Kernel.instance_method(:define_singleton_method).bind(self)
-
-    methods.each do |method|
-      dsm.call(method, ::Kernel.instance_method(method))
+module Blankity
+  # A "blank slate" class which removes _all_ methods (from `BasicObject`) other than the "required"
+  # ones of `__send__` and `__id__`.
+  class BlankSlate < BasicObject
+    # Remove every method except for `__send__` and `__id__`
+    instance_methods.each do |name|
+      undef_method(name) unless name == :__send__ || name == :__id__
     end
 
-    self
-  end
+    # A helper method to define some `Kernel`methods on `self`
+    def __with_Object_methods__(*methods)
+      dsm = ::Object.instance_method(:define_singleton_method).bind(self)
 
-  # Same as `__with_Kernel_method__`, but adds them to subclasses. This shouldn't
-  # be called on `Blankity::BlankSlate` directly, as that affects subclasses
-  def self.__with_Kernel_method__(*methods)
-    if ::Blankity::BlankSlate.equal?(self)
-      raise ArgumentError, 'Cannot call `__with_Kernel_method__` on BlankSlate, as that will affect all blank slates'
+      methods.each do |method|
+        dsm.call(method, ::Object.instance_method(method))
+      end
+
+      self
     end
 
-    methods.each do |method|
-      define_method(method, ::Kernel.instance_method(method))
+    # Same as `__with_Object_methods__`, but adds them to subclasses. This shouldn't
+    # be called on `Blankity::BlankSlate` directly, as that affects subclasses
+    def self.__with_Object_methods__(*methods)
+      if ::Blankity::BlankSlate.equal?(self)
+        raise ArgumentError, 'Cannot call `__with_Object_methods__` on BlankSlate, as that will affect all blank slates'
+      end
+
+      methods.each do |method|
+        define_method(method, ::Kernel.instance_method(method))
+      end
+
+      self
     end
 
-    self
-  end
-
-  # Helper method to create a new `BlankSlate` with a block
-  def self.blank(&block)
-    ::Class.new(self, &block).new
+    # Helper method to create a new `BlankSlate` with a block
+    def self.blank(&block)
+      ::Class.new(self, &block).new
+    end
   end
 end
--- lib/blankity/to.rb
+++ lib/blankity/to.rb
@@ -1,127 +1,129 @@
 # frozen_string_literal: true
 
-module Blankity::To
-  module_function
+module Blankity
+  module To
+    module_function
 
-  # Helper method to create new `BlankSlate`s. Args:
-  # - `to_method`: The method to use, eg `to_i`
-  # - `value`: What to return when `to_method` is called
-  # - `methods`: Optional list of methods from to define, using `value`'s definition
-  # - `hash`: Helper to also add `hash` and `eql?` to `methods`, so it can be used as a hash key
-  # - `kernel_methods`: Optional list of methods from `Kernel` to also define
-  # - `block`: If provided, a block to also run
-  def to_helper(to_method, value, methods: [], kernel_methods: [], hash: false, &block)
-    ::Blankity::BlankSlate.blank {
-      define_method(to_method) { value }
-      __with_Kernel_method__(*kernel_methods)
+    # Helper method to create new `BlankSlate`s. Args:
+    # - `to_method`: The method to use, eg `to_i`
+    # - `value`: What to return when `to_method` is called
+    # - `methods`: Optional list of methods from to define, using `value`'s definition
+    # - `hash`: Helper to also add `hash` and `eql?` to `methods`, so it can be used as a hash key
+    # - `kernel_methods`: Optional list of methods from `Kernel` to also define
+    # - `block`: If provided, a block to also run
+    def to_helper(to_method, value, methods: [], kernel_methods: [], hash: false, &block)
+      ::Blankity::BlankSlate.blank {
+        define_method(to_method) { value }
+        __with_Kernel_method__(*kernel_methods)
 
-      methods |= %i[hash eql?] if hash
-      methods.each do |method|
-        define_method(method, &::Kernel.instance_method(:method).bind_call(value, method))
+        methods |= %i[hash eql?] if hash
+        methods.each do |method|
+          define_method(method, &::Kernel.instance_method(:method).bind_call(value, method))
+        end
+        class_exec(&block) if block
+      }
+    end
+
+    # Create a type which _only_ responds to `.to_i`. See `to_helper` for details.
+    def i(value, ...) = to_helper(:to_i, value.to_i, ...)
+
+    # Create a type which _only_ responds to `.to_int`. See `to_helper` for details.
+    def int(value, ...) = to_helper(:to_int, value.to_int, ...)
+
+    # Create a type which _only_ responds to `.to_s`. See `to_helper` for details.
+    def s(value, ...) = to_helper(:to_s, value.to_s, ...)
+
+    # Create a type which _only_ responds to `.to_str`. See `to_helper` for details.
+    def str(value, ...) = to_helper(:to_str, value.to_str, ...)
+
+    # Create a type which _only_ responds to `.to_a`. See `to_helper` for details.
+    #
+    # This supports `a(1, 2, 3)` as a convenient shorthand for `a([1, 2, 3])`. To
+    # create a `.to_a` that returns an array containing just an array, just use `a([array])`.
+    def a(*ary, **kw, &b)
+      if ary.length == 1
+        to_helper(:to_a, Array(ary[0]), **kw, &b)
+      else
+        to_helper(:to_a, ary, **kw, &b)
       end
-      class_exec(&block) if block
-    }
-  end
-
-  # Create a type which _only_ responds to `.to_i`. See `to_helper` for details.
-  def i(value, ...) = to_helper(:to_i, value.to_i, ...)
-
-  # Create a type which _only_ responds to `.to_int`. See `to_helper` for details.
-  def int(value, ...) = to_helper(:to_int, value.to_int, ...)
-
-  # Create a type which _only_ responds to `.to_s`. See `to_helper` for details.
-  def s(value, ...) = to_helper(:to_s, value.to_s, ...)
-
-  # Create a type which _only_ responds to `.to_str`. See `to_helper` for details.
-  def str(value, ...) = to_helper(:to_str, value.to_str, ...)
-
-  # Create a type which _only_ responds to `.to_a`. See `to_helper` for details.
-  #
-  # This supports `a(1, 2, 3)` as a convenient shorthand for `a([1, 2, 3])`. To
-  # create a `.to_a` that returns an array containing just an array, just use `a([array])`.
-  def a(*ary, **kw, &b)
-    if ary.length == 1
-      to_helper(:to_a, Array(ary[0]), **kw, &b)
-    else
-      to_helper(:to_a, ary, **kw, &b)
     end
-  end
 
-  # Create a type which _only_ responds to `.to_ary`. See `to_helper` for details.
-  #
-  # This supports `ary(1, 2, 3)` as a convenient shorthand for `ary([1, 2, 3])`. To
-  # create a `.to_ary` that returns an array containing just an array, use `ary([array])`.
-  def ary(*ary, **kw, &b)
-    if ary.length == 1
-      to_helper(:to_ary, Array(ary[0]), **kw, &b)
-    else
-      to_helper(:to_ary, ary, **kw, &b)
-    end
-  end
-
-  # Create a type which _only_ responds to `.to_h`. See `to_helper` for details.
-  #
-  # This supports passing in key/values directly via `h('a' => 'b')` as a convenient
-  # shorthand for `h({'a' => 'b'})`, but the shorthand version doesn't allow you
-  # to supply keyowrd arguments that `to_helper` expects. Use `h({'a' => 'b'}, ...)` for that.
-  def h(hash = nil, **kw, &b)
-    if hash
-      to_helper(:to_h, hash, **kw, &b)
-    else
-      to_helper(:to_h, kw, &b)
-    end
-  end
-
-  # Create a type which _only_ responds to `.to_hash`. See `to_helper` for details.
-  #
-  # This supports passing in key/values directly via `h('a' => 'b')` as a convenient
-  # shorthand for `h({'a' => 'b'})`, but the shorthand version doesn't allow you
-  # to supply keyowrd arguments that `to_helper` expects. Use `h({'a' => 'b'}, ...)` for that.
-  def hash(hash = nil, **kw, &b)
-    if hash
-      to_helper(:to_hash, hash, **kw, &b)
-    else
-      to_helper(:to_hash, kw, &b)
-    end
-  end
-
-  # Create a type which _only_ responds to `.to_sym`. See `to_helper` for details.
-  def sym(...) = to_helper(:to_sym, ...)
-
-  # Create a type which _only_ responds to `.to_r`. See `to_helper` for details.
-  def r(...) = to_helper(:to_r, ...)
-
-  # Create a type which _only_ responds to `.to_c`. See `to_helper` for details.
-  def c(...) = to_helper(:to_c, ...)
-
-  # Create a type which _only_ responds to `.to_f`. See `to_helper` for details.
-  def f(...) = to_helper(:to_f, ...)
-
-  # Create a type which _only_ responds to `.to_regexp`. See `to_helper` for details.
-  def regexp(...) = to_helper(:to_regexp, ...)
-
-  # Create a type which _only_ responds to `.to_path`. See `to_helper` for details.
-  def path(...) = to_helper(:to_path, ...)
-
-  # Create a type which _only_ responds to `.to_io`. See `to_helper` for details.
-  def io(...) = to_helper(:to_io, ...)
-
-  # Create a type which _only_ responds to `.begin`, `.end`, and `.exclude_end?`
-  # (the methods required to be considered a "custom range," eg for `Array#[]`.) See
-  # `to_helper` for details.
-  def range(begin_, end_, exclude_end = false, methods: [], kernel_methods: [], hash: false, &block)
-    ::Blankity::BlankSlate.blank {
-      define_method(:begin){ begin_ }
-      define_method(:end){ end_ }
-      define_method(:exclude_end?){ exclude_end }
-
-      __with_Kernel_method__(*kernel_methods)
-
-      methods |= %i[hash eql?] if hash
-      methods.each do |method|
-        define_method(method, &::Kernel.instance_method(:method).bind_call(value, method))
+    # Create a type which _only_ responds to `.to_ary`. See `to_helper` for details.
+    #
+    # This supports `ary(1, 2, 3)` as a convenient shorthand for `ary([1, 2, 3])`. To
+    # create a `.to_ary` that returns an array containing just an array, use `ary([array])`.
+    def ary(*ary, **kw, &b)
+      if ary.length == 1
+        to_helper(:to_ary, Array(ary[0]), **kw, &b)
+      else
+        to_helper(:to_ary, ary, **kw, &b)
       end
-      class_exec(&block) if block
-    }
+    end
+
+    # Create a type which _only_ responds to `.to_h`. See `to_helper` for details.
+    #
+    # This supports passing in key/values directly via `h('a' => 'b')` as a convenient
+    # shorthand for `h({'a' => 'b'})`, but the shorthand version doesn't allow you
+    # to supply keyowrd arguments that `to_helper` expects. Use `h({'a' => 'b'}, ...)` for that.
+    def h(hash = nil, **kw, &b)
+      if hash
+        to_helper(:to_h, hash, **kw, &b)
+      else
+        to_helper(:to_h, kw, &b)
+      end
+    end
+
+    # Create a type which _only_ responds to `.to_hash`. See `to_helper` for details.
+    #
+    # This supports passing in key/values directly via `h('a' => 'b')` as a convenient
+    # shorthand for `h({'a' => 'b'})`, but the shorthand version doesn't allow you
+    # to supply keyowrd arguments that `to_helper` expects. Use `h({'a' => 'b'}, ...)` for that.
+    def hash(hash = nil, **kw, &b)
+      if hash
+        to_helper(:to_hash, hash, **kw, &b)
+      else
+        to_helper(:to_hash, kw, &b)
+      end
+    end
+
+    # Create a type which _only_ responds to `.to_sym`. See `to_helper` for details.
+    def sym(...) = to_helper(:to_sym, ...)
+
+    # Create a type which _only_ responds to `.to_r`. See `to_helper` for details.
+    def r(...) = to_helper(:to_r, ...)
+
+    # Create a type which _only_ responds to `.to_c`. See `to_helper` for details.
+    def c(...) = to_helper(:to_c, ...)
+
+    # Create a type which _only_ responds to `.to_f`. See `to_helper` for details.
+    def f(...) = to_helper(:to_f, ...)
+
+    # Create a type which _only_ responds to `.to_regexp`. See `to_helper` for details.
+    def regexp(...) = to_helper(:to_regexp, ...)
+
+    # Create a type which _only_ responds to `.to_path`. See `to_helper` for details.
+    def path(...) = to_helper(:to_path, ...)
+
+    # Create a type which _only_ responds to `.to_io`. See `to_helper` for details.
+    def io(...) = to_helper(:to_io, ...)
+
+    # Create a type which _only_ responds to `.begin`, `.end`, and `.exclude_end?`
+    # (the methods required to be considered a "custom range," eg for `Array#[]`.) See
+    # `to_helper` for details.
+    def range(begin_, end_, exclude_end = false, methods: [], kernel_methods: [], hash: false, &block)
+      ::Blankity::BlankSlate.blank {
+        define_method(:begin){ begin_ }
+        define_method(:end){ end_ }
+        define_method(:exclude_end?){ exclude_end }
+
+        __with_Kernel_method__(*kernel_methods)
+
+        methods |= %i[hash eql?] if hash
+        methods.each do |method|
+          define_method(method, &::Kernel.instance_method(:method).bind_call(value, method))
+        end
+        class_exec(&block) if block
+      }
+    end
   end
 end
