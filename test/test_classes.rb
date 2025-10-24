b2d3cc6acff46bfbf37650e3e072e8a8b1cbcd3e
SamW <mail@sampersand.me>
2025-10-24 10:18:26 -0700

    add tests

--- test/test_classes.rb
+++ test/test_classes.rb
@@ -25,66 +25,124 @@ class TestBlankity_Classes < Minitest::Test
 
   def test_ToI
     assert_basic_class_works Blankity::ToI, :to_i, 12
+
+    # Test it with a builtin method
+    assert_equal 3, Integer(Blankity::ToI.new(3))
   end
 
   def test_ToInt
     assert_basic_class_works Blankity::ToInt, :to_int, 12
+
+    # Test it with a builtin method
+    assert_equal [4, 4, 4], [4] * Blankity::ToInt.new(3)
   end
 
   def test_ToS
     assert_basic_class_works Blankity::ToS, :to_s, 'str'
+
+    # Test it with a builtin method
+    assert_equal '<hi>', "<#{Blankity::ToS.new('hi')}>"
   end
 
   def test_ToStr
     assert_basic_class_works Blankity::ToStr, :to_str, 'str'
+
+    # Test it with a builtin method
+    assert_equal 'hi world', 'hi' + Blankity::ToStr.new(' world')
   end
 
   def test_ToA
     assert_basic_class_works Blankity::ToA, :to_a, [1, 2]
+
+    # Test it with a builtin method
+    assert_equal [3, 4, 5], [*Blankity::ToA.new([3, 4, 5])]
   end
 
   def test_ToAry
     assert_basic_class_works Blankity::ToAry, :to_ary, [1, 2]
+
+    # Test it with a builtin method
+    assert_equal '3 hi', '%d %s' % Blankity::ToAry.new([3, 'hi'])
   end
 
   def test_ToH
     assert_basic_class_works Blankity::ToH, :to_h, { 'a' => 'b' }
+
+    # (interestingly, there _are_ no ruby stdlib things that use `.to_h`)
   end
 
   def test_ToHash
     assert_basic_class_works Blankity::ToHash, :to_hash, { 'a' => 'b' }
+
+    # Test it with a builtin method
+    assert_equal '3 hi', '%<a>d %<b>s' % Blankity::ToHash.new({ a: 3, b: 'hi' })
   end
 
   def test_ToSym
     assert_basic_class_works Blankity::ToSym, :to_sym, :symbol
+
+    # Test it with a builtin method
+    category = Warning.categories.first or fail 'no warning categories?'
+    was_enabled = Warning[category]
+    out, err = capture_io do
+      Warning[category] = true
+      warn 'oops', category: Blankity::ToSym.new(category)
+      Warning[category] = was_enabled
+    end
+
+    assert_equal '', out
+    assert_equal "oops\n", err
   end
 
   def test_ToR
     assert_basic_class_works Blankity::ToR, :to_r, 12r
+
+    # Test it with a builtin method
+    assert_equal 2r, Rational(Blankity::ToR.new(2r))
   end
 
   def test_ToC
     assert_basic_class_works Blankity::ToC, :to_c, 34i
+
+    # Test it with a builtin method
+    assert_equal 3i, Complex(Blankity::ToC.new(3i))
   end
 
   def test_ToF
     assert_basic_class_works Blankity::ToF, :to_f, 5.6
+
+    # Test it with a builtin method
+    assert_equal 3.0, Regexp.new('x', timeout: Blankity::ToF.new(3.0)).timeout
   end
 
   def test_ToRegexp
     assert_basic_class_works Blankity::ToRegexp, :to_regexp, /regex/
+
+    # Test it with a builtin method
+    assert_equal %r/test/, Regexp.try_convert(Blankity::ToRegexp.new(/test/))
   end
 
   def test_ToPath
     assert_basic_class_works Blankity::ToPath, :to_path, 'path'
+
+    # Test it with a builtin method
+    assert File.exist? Blankity::ToPath.new(__FILE__)
   end
 
   def test_ToIO
     assert_basic_class_works Blankity::ToIO, :to_io, $stdout
+
+    # Test it with a builtin method
+    assert File.exist? Blankity::ToIO.new($stdout)
   end
 
   def test_ToProc
     assert_basic_class_works Blankity::ToProc, :to_proc, proc{}
+
+    # Test it with a builtin method
+    h = Hash.new
+    h.default_proc = Blankity::ToProc.new(proc { |hash, key| key + 3 })
+    assert_equal 4, h[1]
   end
 
   def test_Range
@@ -115,5 +173,8 @@ class TestBlankity_Classes < Minitest::Test
       cls.new(10, 20, true, methods: %i[then], hash: true){ def five = hash*0 + 5 }
          .then { |instance| [instance.begin, instance.end, instance.exclude_end?, instance.five] }
     )
+
+    # Test it with a builtin method
+    assert_equal %w[b c], %w[a b c d][Blankity::Range.new(1, 3, true)]
   end
 end
---
+++ test/test_to.rb
@@ -0,0 +1,173 @@
+require 'test_helper'
+
+class TestBlankity_To < Minitest::Test
+  def assert_to_method_works(method, cls, expected)
+    instance = Blankity::To.public_send(method, expected, methods: %i[instance_of?]) {
+      def hello = 10
+    }
+
+    # Make sure it returns a `cls`
+    assert_instance_of cls, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal expected, instance.__send__(:"to_#{method}")
+
+    # Make sure you can feed it into itself
+    assert_equal expected, Blankity::To.public_send(method, instance).__send__(:"to_#{method}")
+  end
+
+  def test_i
+    assert_to_method_works :i, Blankity::ToI, 1
+  end
+
+  def test_int
+    assert_to_method_works :int, Blankity::ToInt, 1
+  end
+
+  def test_s
+    assert_to_method_works :s, Blankity::ToS, 'str'
+  end
+
+  def test_str
+    assert_to_method_works :str, Blankity::ToStr, 'str'
+  end
+
+  def test_a
+    instance = Blankity::To.a(1, 2, 3, methods: %i[instance_of?]) { def hello = 10 }
+
+    # Make sure it returns a `cls`
+    assert_instance_of Blankity::ToA, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal [1, 2, 3], instance.to_a
+
+    # Make sure you can feed it into itself
+    assert_equal [1, 2, 3], Blankity::To.a(*instance).to_a
+  end
+
+  def test_ary
+    instance = Blankity::To.ary(1, 2, 3, methods: %i[instance_of?]) { def hello = 10 }
+
+    # Make sure it returns a `cls`
+    assert_instance_of Blankity::ToAry, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal [1, 2, 3], instance.to_ary
+
+    # Make sure you can feed it into itself
+    assert_equal [1, 2, 3], Blankity::To.ary(*instance.to_ary).to_ary
+  end
+
+  def test_h
+    instance = Blankity::To.h({ 'a' => 'b' }, methods: %i[instance_of?]) { def hello = 10 }
+
+    # Make sure it returns a `cls`
+    assert_instance_of Blankity::ToH, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal Hash['a' => 'b'], instance.to_h
+
+    # Make sure you can feed it into itself
+    assert_equal Hash['a' => 'b'], Blankity::To.h(instance.to_h).to_h
+
+    # Make sure the non-method one works too
+    instance2 = Blankity::To.h('c' => 'd', methods: %i[instance_of?]) { def hello = 10 }
+    assert_equal 10, instance2.hello
+    assert_equal Hash['c' => 'd', methods: %i[instance_of?]], instance2.to_h
+    assert_equal Hash['c' => 'd', methods: %i[instance_of?]], Blankity::To.h(instance2.to_h).to_h
+  end
+
+
+  def test_hash
+    instance = Blankity::To.hash({ 'a' => 'b' }, methods: %i[instance_of?]) { def hello = 10 }
+
+    # Make sure it returns a `cls`
+    assert_instance_of Blankity::ToHash, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal Hash['a' => 'b'], instance.to_hash
+
+    # Make sure you can feed it into itself
+    assert_equal Hash['a' => 'b'], Blankity::To.hash(instance.to_hash).to_hash
+
+    # Make sure the non-method one works too
+    instance2 = Blankity::To.hash('c' => 'd', methods: %i[instance_of?]) { def hello = 10 }
+    assert_equal 10, instance2.hello
+    assert_equal Hash['c' => 'd', methods: %i[instance_of?]], instance2.to_hash
+    assert_equal Hash['c' => 'd', methods: %i[instance_of?]], Blankity::To.hash(instance2.to_hash).to_hash
+  end
+
+  def test_sym
+    assert_to_method_works :sym, Blankity::ToSym, :symbol
+  end
+
+  def test_r
+    assert_to_method_works :r, Blankity::ToR, 1r
+  end
+
+  def test_c
+    assert_to_method_works :c, Blankity::ToC, 1i
+  end
+
+  def test_f
+    assert_to_method_works :f, Blankity::ToF, 1.0
+  end
+
+  def test_regexp
+    # lol for some reason `Regexp` doesn't define `.to_regexp`...
+    my_regex = Regexp.new('regex')
+    def my_regex.to_regexp = self
+
+    assert_to_method_works :regexp, Blankity::ToRegexp, my_regex
+  end
+
+  def test_path
+    assert_to_method_works :path, Blankity::ToPath, 'path'
+  end
+
+  def test_io
+    assert_to_method_works :io, Blankity::ToIO, $stdout
+  end
+
+  def test_proc
+    assert_to_method_works :proc, Blankity::ToProc, proc { 3 }
+
+    # Make sure you can pass a block
+    instance = Blankity::To.proc(methods: %i[instance_of?]) { 3 }
+    assert_instance_of Blankity::ToProc, instance
+    assert_equal 3, instance.to_proc.call
+    assert_equal 3, Blankity::To.proc(instance).to_proc.call
+  end
+
+  def test_range
+    instance = Blankity::To.range(10, 20, true, methods: %i[instance_of?]) {
+      def hello = 10
+    }
+
+    # Make sure it returns a `cls`
+    assert_instance_of Blankity::Range, instance
+
+    # Make sure you can pass procs
+    assert_equal 10, instance.hello
+
+    # Make sure the method actually works
+    assert_equal 10, instance.begin
+    assert_equal 20, instance.end
+    assert_equal true, instance.exclude_end?
+  end
+end
